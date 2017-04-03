b4eb56856fab130bb6c20c0527d8a21803467ce1
Idelier <enevester@gmail.com>
2017-04-03 16:20:48 +0200

    Update Datalogger1.ino

--- Datalogger1.ino
+++ Datalogger1.ino
@@ -3,28 +3,40 @@ unsigned long tslr = 0; // time since last reset
 int interval = 1000; //interval of 1 s
 long count = 0; // parameter for number of counts
 
-void count()
+void count_inc()
 {
   count++;
 }
 
+void count_start()
+{
+  attachInterrupt(0, counting_stop, FALLING); // ready to stop
+  attachInterrupt(1, count_inc, RISING); // ready to count pulses
+}
+
+void count_stop()
+{
+  detachInterrupt(1); // stop counting pulses
+  attachInterrupt(0, counting_start, RISING); // ready to start
+}
+
+
 
 void setup() 
 {
+  PinMode(3, INPUT);
+  digitalWrite(3, HIGH);
   Serial.begin(9600);
   delay(1000);  // delay to remove random voltage spike
 }
 
 void loop() 
 {
-  receiveddata = analogRead(A5);
+  
   tslr = millis(); // obtaining reference
    while((millis() - tslr) < interval)
    {
-    if(receiveddata == 5 || receiveddata == -5)  //Check if received signal if yes then add count
-    {
-       count();
-    } 
+    count_start();     
    }
    Serial.println(count); //Printing the count that came from last 1s
    count = 0; // Zeroing the count for next loop
