89835eb20105801e8836c3a4c51f127d52090312
Idelier <enevester@gmail.com>
2017-06-28 19:29:31 +0200

    Update Datalogger1.ino

--- Datalogger1.ino
+++ Datalogger1.ino
@@ -1,38 +1,30 @@
 //#This program is a part of Geiger Muller Pulse Counter working together with java app.
 //# by K.D.
 
-unsigned long tslr = 0;                    // time since last reset
-int interval = 5000;                       // interval of x time
-long count = 0;                            // parameter for number of counts
-volatile int state = LOW;                 // parameter for LED
-
+unsigned long tslr = 0;                   // time since last reset
+int interval = 1000;                      // interval of x time
+volatile int count = 0;                  // parameter for number of counts
+int blinkCount = 0;
+int state = LOW;                 // parameter for LED
 
 void count_inc()
 {
   ++count;                                //increase count by 1
-  flash_led();                            //change the state of onboard LED to signal count change
 }
 
 void print_count()
 {
-  Serial.print(count);                  //  Printing the count that came from last time frame
-  Serial.println(";");
-   count = 0;                             //  Zeroing the count for next loop
+  Serial.println(count);                    //  Printing the count that came from last time frame
+  count = 0;                             //  Zeroing the count for next loop
 }
 void flash_led()
 {
   state = !state;                         //change led state
+  digitalWrite(13, state);
 }
 
-void count_start()
+void setup()
 {
-  attachInterrupt(0, count_inc, FALLING);   //  add a count 
-  //detachInterrupt(0);                     //  stop counting pulse
-
-}
-
-void setup() 
-{ 
   pinMode(2, INPUT);                      //  attach counting pin
   pinMode(13, OUTPUT);                     //  attach onboard LED
   digitalWrite(13, state);
@@ -40,13 +32,18 @@ void setup()
   delay(1000);                            //  delay to remove random voltage spikes
 }
 
-void loop() 
+void loop()
 {
-  
-  tslr = millis();                        //  obtaining reference
-   while((millis() - tslr) < interval)    //  counting time frame
-   {
-    count_start();     
-   }
-   print_count();                        
+  tslr = millis();//  obtaining reference
+  attachInterrupt(0, count_inc, FALLING); 
+  while ((millis() - tslr) < interval)   //  counting time frame
+  {
+    if (blinkCount <= count)
+    {
+      flash_led();      //change the state of onboard LED to signal count change
+      ++blinkCount;
+    }
+  }
+  detachInterrupt(0);   
+  print_count();
 }
